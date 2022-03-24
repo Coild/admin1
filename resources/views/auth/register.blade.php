6abe54a4c918c69e29bf78008ef921573c443404
a <a@a.a>
2022-03-24 10:01:08 +0800

    register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -46,28 +46,38 @@
                                             <input class="form-control" name="username" id="inputEmail" type="text" placeholder="name@example.com" />
                                             <label for="inputEmail">Username</label>
                                         </div>
-                                        <div class="form-floating mb-3">
+                                        <!-- <div class="form-floating mb-3">
                                             <div classs="form-group">
                                                 <input type="text" id="search" name="search" class="form-control" />
                                             </div>
-                                        </div>
-                                        <div class="row mb-3">
-                                            <div class="col-md-6">
-                                                <div class="form-floating mb-3 mb-md-0">
-                                                    <input class="form-control" name="password" id="inputPassword" type="password" placeholder="Create a password" />
-                                                    <label for="inputPassword">Password</label>
+                                        </div> -->
+                                        <div class="form-group row">
+                                            <label for="inputEmail3" class="col-sm-2 col-form-label">Waktu Pembersihan</label>
+                                            <div class="col-sm-10">
+                                                <select class="form-control" name="search" id="inlineFormCustomSelect">
+                                                    <option selected>Choose...</option>
+                                                    @foreach($data as $row)
+                                                    <option value="1">{{$row['nama']}}</option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                            <div class="row mb-3">
+                                                <div class="col-md-6">
+                                                    <div class="form-floating mb-3 mb-md-0">
+                                                        <input class="form-control" name="password" id="inputPassword" type="password" placeholder="Create a password" />
+                                                        <label for="inputPassword">Password</label>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-floating mb-3 mb-md-0">
+                                                        <input class="form-control" id="inputPasswordConfirm" type="password" placeholder="Confirm password" />
+                                                        <label for="inputPasswordConfirm">Confirm Password</label>
+                                                    </div>
                                                 </div>
                                             </div>
-                                            <div class="col-md-6">
-                                                <div class="form-floating mb-3 mb-md-0">
-                                                    <input class="form-control" id="inputPasswordConfirm" type="password" placeholder="Confirm password" />
-                                                    <label for="inputPasswordConfirm">Confirm Password</label>
-                                                </div>
+                                            <div class="mt-4 mb-0">
+                                                <div class="d-grid"><button type="submit" class="btn btn-primary btn-block" href="/">Create Account</button></div>
                                             </div>
-                                        </div>
-                                        <div class="mt-4 mb-0">
-                                            <div class="d-grid"><button type="submit" class="btn btn-primary btn-block" href="/">Create Account</button></div>
-                                        </div>
                                     </form>
                                 </div>
                                 <div class="card-footer text-center py-3">
