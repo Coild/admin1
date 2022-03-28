6f815872d9d1ac3a6604f22a143de9f7f8e4b010
coild <xreza75@gmail.com>
2022-03-29 02:01:27 +0800

    model audit

--- app/Http/Controllers/auditor.php
+++ app/Http/Controllers/auditor.php
@@ -2,23 +2,35 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
+use App\Models\{User,Pabrik,laporan};
 use Illuminate\Http\Request;
 
 class auditor extends Controller
 {
     public function list_pabrik () {
 
-        $data = User::all('level')->distinct();
-        dd($data);
-        return view('auditor.listpabrik');
+        $data = pabrik::all();
+
+        // dd($data);
+        return view('auditor.listpabrik',['pabrik'=>$data]);
     }
 
-    public function list_dokumen () {
-        return view('auditor.listdokumen');
+    public function list_dokumen ($id) {
+        $data = laporan::all()->where('laporan_batch',$id);
+        return view('auditor.listdokumen',['data'=>$data]);
+    }
+
+    public function list_batch ($id) {
+        // dd($id);
+        $data = laporan::all()->where('pabrik_id',$id);
+        return view('auditor.listbatch' ,  ['data' => $data]);
     }
 
     public function list_request () {
+        return view('auditor.request');
+    }
+
+    public function ajukan_request () {
         return view('auditor.request');
     }
 }
