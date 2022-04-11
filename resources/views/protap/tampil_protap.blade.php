7613ed913d0f6f308598a48a29f814abbcda7c5a
Your Name <you@example.com>
2022-04-11 11:04:00 +0800

    protap

--- resources/views/protap/tampil_protap.blade.php
+++ resources/views/protap/tampil_protap.blade.php
@@ -135,7 +135,7 @@
                             <thead>
                                 <tr>
                                     <th scope="col">No</th>
-                                    <th scope="col">Nama DIP</th>
+                                    <th scope="col">Nama PROTAP</th>
                                     <th scope="col">Action</th>
                                 </tr>
                             </thead>
