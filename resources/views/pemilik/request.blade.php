d80b363405dbd3008ccd65b2501780ed8a724029
Your Name <you@example.com>
2022-06-30 22:27:33 +0800

    daftar audit

--- resources/views/pemilik/request.blade.php
+++ resources/views/pemilik/request.blade.php
@@ -6,7 +6,7 @@
 @section('content')
 <main>
     <div class="container-fluid px-4">
-        <h1 class="mt-4">Pengolahan Batch</h1>
+        <h1 class="mt-4">Daftar Audit</h1>
         <ol class="breadcrumb mb-4">
             <li class="breadcrumb-item active">Daftar audit</li>
         </ol>
