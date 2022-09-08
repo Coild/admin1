7d4808004f12557d10beff44a3323015b27d436b
Coild <xreza75@gmail.com>
2022-09-08 20:03:48 +0800

    https boot v2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         /* Use https links instead http links */
-        if (Request::server('HTTP_X_FORWARDED_PROTO') == 'https') {
+        if (env('APP_ENV' !== 'local')) {
             URL::forceScheme('https');
         }
     }
